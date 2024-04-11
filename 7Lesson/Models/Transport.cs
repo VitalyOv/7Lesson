bdb540fd88b331efd827373cee0c1a6f8e520eca
VitalyOv <vit2013aly@gmail.com>
2024-04-11 20:09:25 +0300

    Add project files.

---
+++ 7Lesson/Models/Transport.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7Lesson.Models
+{
+    public class Boat: IMovable
+    {
+        public string Name { get; set; }
+
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ 7Lesson/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using _7Lesson;
+using _7Lesson.Models;
+
+var travel = new Travel(new Car());
+travel.GoToTravel();
+travel.SetTransport(new Boat());
+travel.GoToTravel();
+
+public class Travel
+{
+    public string Name = "123";
+    private IMovable _movable;
+
+    public Travel(IMovable movable)
+    {
+        _movable = movable;
+    }
+
+    public void SetTransport(IMovable movable)
+    {
+        _movable = movable;
+    }
+
+
+    public void GoToTravel()
+    {
+        _movable.Move();
+    }
+}
